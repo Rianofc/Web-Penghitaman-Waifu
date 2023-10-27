dedde3e376ffbb91dae812059b73022cac19ef81
Prozycal <93260896+Prozycal@users.noreply.github.com>
2023-10-27 20:52:21 +0700

    Add files via upload

--- script2.js
+++ script2.js
@@ -17,10 +17,12 @@ function searchFunction() {
     if (query.includes("elysia")) {
         window.location.href = 'index.html';
     } else if (query.includes("ryo")) {
+        window.location.href = 'page2.html';
     } else if (query.includes("mahiru shiina")) {
         window.location.href = 'page3.html';
     } else {
         alert('Waifu tidak ditemukan');
     }
 }
+
 
