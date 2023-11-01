d42e283d4d6b323028df1bbfcef6cf0896764ed2
Prozycal <fictorofficial9@gmail.com>
2023-11-01 16:09:24 +0700

    commit

--- scripts/shizuka.js
+++ scripts/shizuka.js
@@ -17,47 +17,7 @@ document.getElementById('searchBar').addEventListener('keyup', function(event) {
     }
 });
 
-function searchFunction() {
-    const query = document.getElementById('searchBar').value.toLowerCase();
 
-    if (query.includes("elysia")) {
-        window.location.href = '/pages/elysia.html';
-    } else if (query.includes("ryo")) {
-        window.location.href = '/pages/ryo-yamada.html';
-    } else if (query.includes("mahiru")) {
-        window.location.href = '/pages/mahiru-shiina.html';
-    } else if (query.includes("zeta")) {
-        window.location.href = '/pages/vestia-zeta.html';
-    } else if (query.includes("bocchi")) {
-        window.location.href = '/pages/bocchi.html';
-    } else if (query.includes("kafka")) {
-        window.location.href = '/pages/kafka.html';
-    } else if (query.includes("hutao")) {
-        window.location.href = '/pages/hu-tao.html';
-    } else if (query.includes("hu tao")) {
-        window.location.href = '/pages/hu-tao.html';
-    } else if (query.includes("nijika")) {
-        window.location.href = '/pages/nijika.html';
-    } else if (query.includes("kita")) {
-        window.location.href = '/pages/kita.html';
-    } else if (query.includes("ikuyo kita")) {
-        window.location.href = '/pages/kita.html';
-    } else if (query.includes("kobo")) {
-        window.location.href = '/pages/kobo.html';
-    } else if (query.includes("kobo kanaeru")) {
-        window.location.href = '/pages/kobo.html';
-    } else if (query.includes("elaina")) {
-        window.location.href = '/pages/elaina.html';
-    } else if (query.includes("shizuka")) {
-        window.location.href = '/pages/shizuka.html';
-    } else if (query.includes("shizuka mikazuki")) {
-        window.location.href = '/pages/shizuka.html';
-    } else if (query.includes("yae miko")) {
-        window.location.href = '/pages/yae-miko.html';
-    } else {
-        modal.style.display = "block";
-    }
-}
 
 
 function showDropdown() {
